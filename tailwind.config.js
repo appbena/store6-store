8bb6a190d2491a9e60f02d196e23037af0a6998d
appbena <hassan@sheikh.ly>
2025-09-18 22:07:34 +0200

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#007bff",
+        secondary: "#6c757d",
+        background: "#ffffff",
+      },
+      fontFamily: {
+        sans: ['Cairo', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
